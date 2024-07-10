77910564c28460317b94a4cbdf8f61cf444b5b81
Dolapo001 <adedolapovictoria927@gmail.com>
2024-07-10 12:59:51 +0100

    RescheduleAppointmentView

--- appointment/views.py
+++ appointment/views.py
@@ -24,3 +24,37 @@ class ScheduleAppointmentView(APIView):
             return Response(data, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
+class RescheduleAppointmentView(APIView):
+    serializer_class = RescheduleAppointmentSerializer
+
+    def put(self, request, ticket_number):
+        try:
+            appointment = Appointment.objects.get(ticket_number=ticket_number)
+        except Appointment.DoesNotExist:
+            return Response({"error": "Appointment not found"}, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = self.serializer_class(appointment, data=request.data)
+        if serializer.is_valid():
+            new_date = serializer.validated_data.get('date')
+            new_time = serializer.validated_data.get('time')
+            new_stylist_id = serializer.validated_data.get('stylist')
+
+            if new_date:
+                appointment.date = new_date
+            if new_time:
+                appointment.time = new_time
+            if new_stylist_id:
+                try:
+                    new_stylist = Stylist.objects.get(id=new_stylist_id)
+                    appointment.stylist = new_stylist
+                except Stylist.DoesNotExist:
+                    return Response({"error": "Stylist not found"}, status=status.HTTP_404_NOT_FOUND)
+
+            appointment.status = 'rescheduled'
+            appointment.save()
+
+            # Confirmation message logic can be added here if needed
+
+            return Response(ConfirmAppointmentSerializer(appointment).data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
