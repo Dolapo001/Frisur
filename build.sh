286275f1ba97db725dc62a030f85724273888a1d
Dolapo001 <adedolapovictoria927@gmail.com>
2024-07-23 20:04:25 +0100

    new updates

--- build.sh
+++ build.sh
@@ -8,6 +8,6 @@ pip3 install -r requirements.txt
 
 python3.12 manage.py collectstatic --noinput
 
- celery -A barbing_salon worker --loglevel=info
+celery -A barbing_salon worker --loglevel=info
 
 echo "  BUILD END"
