373f1205974b699072a912adcdf3d37d35e6ce5f
Dolapo001 <adedolapovictoria927@gmail.com>
2024-07-27 12:19:27 +0100

    new updates

--- barbing_salon/settings.py
+++ barbing_salon/settings.py
@@ -32,7 +32,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 
 DEBUG = os.getenv('DEBUG') == 'True'
-ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = ['*']
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
