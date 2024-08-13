9db5a6c4b33af9039b2e085fa4d3000e0bf24a7c
Dolapo001 <adedolapovictoria927@gmail.com>
2024-08-13 10:44:19 +0100

    new updates

--- barbing_salon/settings.py
+++ barbing_salon/settings.py
@@ -245,11 +245,11 @@ SITE_DOMAIN = 'http://localhost:5173'
 
 #CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
-    'http://localhost:5173',
+    'http://localhost:5173/',
     'https://frisur-barbing-salon.onrender.com',
     'http://127.0.0.1:8000/'
-
 ]
+
 
 CORS_ALLOW_METHODS = (
     "DELETE",
