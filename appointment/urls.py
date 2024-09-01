83c6fafb997d5666484a6738bf85bc0ebfcc66e2
Dolapo001 <adedolapovictoria927@gmail.com>
2024-09-01 13:23:50 +0100

    new updates

--- appointment/urls.py
+++ appointment/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import ScheduleAppointmentView, RescheduleAppointmentView, CancelAppointmentView, ConfirmAppointmentView
 
 urlpatterns = [
-    path('schedule', ScheduleAppointmentView.as_view(), name='schedule-appointment'),
-    path('<str:ticket_number>/reschedule', RescheduleAppointmentView.as_view(), name='reschedule-appointment'),
-    path('<str:ticket_number>/cancel', CancelAppointmentView.as_view(), name='cancel-appointment'),
-    path('<str:ticket_number>/confirm', ConfirmAppointmentView.as_view(), name='confirm-appointment'),
+    path('schedule/', ScheduleAppointmentView.as_view(), name='schedule-appointment'),
+    path('<str:ticket_number>/reschedule/', RescheduleAppointmentView.as_view(), name='reschedule-appointment'),
+    path('<str:ticket_number>/cancel/', CancelAppointmentView.as_view(), name='cancel-appointment'),
+    path('<str:ticket_number>/confirm/', ConfirmAppointmentView.as_view(), name='confirm-appointment'),
 ]
